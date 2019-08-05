6c6ae8b820699ca909bff8d0bb7e13989365194b
Daniel <ddazaj@uninorte.edu.co>
2019-08-05 11:37:08 -0500

    update

--- src/laserDataReceiver.py
+++ src/laserDataReceiver.py
@@ -6,7 +6,7 @@ import json
 import yaml
 
 def LaserReceiver():
-    pub = rospy.Publisher('scanPi', LaserScan, queue_size=10)
+    pub = rospy.Publisher('scan', LaserScan, queue_size=10)
     rospy.init_node('LaserReceiver', anonymous=True)
     rate = rospy.Rate(10) # 10hz
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
@@ -14,10 +14,14 @@ def LaserReceiver():
     while not rospy.is_shutdown():
         data, addr = sock.recvfrom(15024)
         data = json.loads(data)
+        intensities = data["intensities"]
+        intensities[:] = [float(x) for x in intensities]
+        ranges = data["ranges"]
+        ranges[:] = [float(x) for x in ranges]
         laser = LaserScan()
         laser.header.stamp.secs = data["header"]["stamp"]["secs"]
         laser.header.stamp.nsecs = data["header"]["stamp"]["nsecs"]
-        laser.header.frame_id = data["header"]["frame_id"]
+        laser.header.frame_id = data["header"]["frame_id"].encode("ascii")
         laser.header.seq = data["header"]["seq"]
         laser.angle_min = data["angle_min"]
         laser.angle_max = data["angle_max"]
@@ -26,8 +30,8 @@ def LaserReceiver():
         laser.range_min = data["range_min"]
         laser.range_max = data["range_max"]
         laser.scan_time = data["scan_time"]
-        laser.ranges = data["ranges"]
-        laser.intensities = data["intensities"]
+        laser.ranges = ranges
+        laser.intensities = intensities
         pub.publish(laser)
         rate.sleep()
 
--- src/odomDataReceiver.py
+++ src/odomDataReceiver.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 import rospy
-from sensor_msgs.msg import LaserScan
+from nav_msgs.msg import Odometry
 import socket
 import json
 import yaml
 
 def OdomReceiver():
-    pub = rospy.Publisher('scanPiOdom', LaserScan, queue_size=10)
+    pub = rospy.Publisher('odom', Odometry, queue_size=10)
     rospy.init_node('OdomReceiver', anonymous=True)
     rate = rospy.Rate(10) # 10hz
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
@@ -14,9 +14,28 @@ def OdomReceiver():
     while not rospy.is_shutdown():
         data, addr = sock.recvfrom(15024)
         data = json.loads(data)
-        print data
-        #laser = LaserScan()
-        #pub.publish(laser)
+        odom = Odometry()
+        odom.twist.twist.linear.y = data["twist"]["twist"]["linear"]["y"]
+        odom.twist.twist.linear.x = data["twist"]["twist"]["linear"]["x"]
+        odom.twist.twist.linear.z = data["twist"]["twist"]["linear"]["z"]
+        odom.twist.twist.angular.y = data["twist"]["twist"]["angular"]["y"]
+        odom.twist.twist.angular.x = data["twist"]["twist"]["angular"]["x"]
+        odom.twist.twist.angular.z = data["twist"]["twist"]["angular"]["z"]
+        odom.twist.covariance = data["twist"]["covariance"]
+        odom.header.stamp.secs = data["header"]["stamp"]["secs"]
+        odom.header.stamp.nsecs = data["header"]["stamp"]["nsecs"]
+        odom.header.frame_id = data["header"]["frame_id"]
+        odom.header.seq = data["header"]["seq"]
+        odom.pose.pose.position.x = data["pose"]["pose"]["position"]["x"]
+        odom.pose.pose.position.y = data["pose"]["pose"]["position"]["y"]
+        odom.pose.pose.position.z = data["pose"]["pose"]["position"]["z"]
+        odom.pose.pose.orientation.x = data["pose"]["pose"]["orientation"]["x"]
+        odom.pose.pose.orientation.y = data["pose"]["pose"]["orientation"]["y"]
+        odom.pose.pose.orientation.z = data["pose"]["pose"]["orientation"]["z"]
+        odom.pose.pose.orientation.w = data["pose"]["pose"]["orientation"]["w"]
+        odom.pose.covariance = data["pose"]["covariance"]
+        odom.child_frame_id = data["child_frame_id"]
+        pub.publish(odom)
         rate.sleep()
 
 if __name__ == '__main__':
